cac24f46d62edef9a3212b6c5d1e87048aee5579
Deepak Kumar <tejpratapyadav263@gmail.com>
2024-09-24 11:29:43 +0530

    i am using Register Page

--- lib/RegisterPage.dart
+++ lib/RegisterPage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class Registerpage extends StatefulWidget {
   const Registerpage({super.key});
-
   @override
   State<Registerpage> createState() => _RegisterpageState();
 }
@@ -10,6 +9,29 @@ class Registerpage extends StatefulWidget {
 class _RegisterpageState extends State<Registerpage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {},
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:20.0),
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+            ),
+            SizedBox(height: 30,),
+            TextField(
+              keyboardType: TextInputType.phone,
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ninemedicine/RegisterPage.dart';
 
 void main (){
   runApp(MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Registerpage(),
     );
   }
 }
