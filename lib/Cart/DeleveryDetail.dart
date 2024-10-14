6535b9418ef5ac69a1dfdc148e21ad96a580f2ee
Deepak Kumar <tejpratapyadav263@gmail.com>
2024-10-14 11:49:55 +0530

    Merge remote-tracking branch 'origin/LoginBranch' into LoginBranch

---
+++ lib/Cart/DeleveryDetail.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Deleverydetail extends StatefulWidget {
+
+
+  const Deleverydetail({super.key});
+
+  @override
+  State<Deleverydetail> createState() => _DeleverydetailState();
+}
+
+class _DeleverydetailState extends State<Deleverydetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
