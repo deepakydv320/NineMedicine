0c081db54eac5607bac04faba67b094ec21482e6
Suraj Samrat <surajsamrat762@gmail.com>
2024-10-14 12:03:37 +0530

    Profile HomePage Health Articles Manage patients Reminder Tm wallet Refer and Earn Location And help setting

--- lib/RegisterPage.dart
+++ lib/RegisterPage.dart
@@ -1,19 +1,239 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ninemedicine/login_page.dart';
 
-class Registerpage extends StatefulWidget {
-  const Registerpage({super.key});
-
+class RegisterScreen extends StatefulWidget {
   @override
-  State<Registerpage> createState() => _RegisterpageState();
+  _RegisterScreenState createState() => _RegisterScreenState();
 }
 
-class _RegisterpageState extends State<Registerpage> {
+class _RegisterScreenState extends State<RegisterScreen> {
+  late double screenHeight;
+  bool _isRegistering = false;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.amberAccent,
-    ),
+      body: Column(
+        children: [
+          Container(
+            height: screenHeight * 0.35,
+            decoration: BoxDecoration(
+              color: Colors.green.shade900,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(80),
+              ),
+            ),
+            child: Center(
+              child: Image.asset(
+                'assets/logo.png',
+                height: screenHeight * 0.25,
+                width: screenHeight * 0.25,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(24.0),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _buildRegisterForm(),
+                      SizedBox(height: 20),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginScreen(),
+                            ),
+                          );
+                        },
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "Already a member? ",
+                                style: TextStyle(color: Colors.black54),
+                              ),
+                              TextSpan(
+                                text: "Login",
+                                style: TextStyle(color: Colors.black54),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  Widget _buildRegisterForm() {
+    return Column(
+      children: [
+        SizedBox(height: 13),
+        TextFormField(
+          controller: _nameController,
+          decoration: InputDecoration(
+            labelText: 'Fullname',
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        TextFormField(
+          controller: _emailController,
+          decoration: InputDecoration(
+            labelText: 'Email',
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        TextFormField(
+          controller: _phoneController,
+          decoration: InputDecoration(
+            labelText: 'Phone Number',
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        TextFormField(
+          controller: _passwordController,
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Password',
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+            backgroundColor: Colors.green.shade900,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          onPressed: () {
+            _registerUser();
+          },
+          child: Text('REGISTER', style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    );
+  }
+
+  // Future<void> _registerUser() async {
+  //   try {
+  //     UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+  //       email: _emailController.text,
+  //       password: _passwordController.text,
+  //     );
+  //     await _firestore.collection('users').doc().set({
+  //       'name': _nameController.text,
+  //       'email': _emailController.text,
+  //       'phone': _phoneController.text,
+  //     });
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(content: Text('Registration successful')),
+  //     );
+  //     Navigator.pushReplacement(
+  //       context,
+  //       MaterialPageRoute(
+  //         builder: (context) => LoginScreen(),
+  //       ),
+  //     );
+  //   } on FirebaseAuthException catch (e) {
+  //     if (e.code == 'email-already-in-use') {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(content: Text('Email is already registered')),
+  //       );
+  //     } else {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(content: Text('Registration failed')),
+  //       );
+  //     }
+  //   }
+  // }
+
+
+  Future<void> _registerUser() async {
+    if (_isRegistering) return;
+
+    setState(() {
+      _isRegistering = true;
+    });
+
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+
+      await _firestore.collection('User').doc(userCredential.user!.uid).set({
+        'name': _nameController.text,
+        'email': _emailController.text,
+        'phone': _phoneController.text,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Registration successful')),
+      );
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Email is already registered')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Registration failed')),
+        );
+      }
+    } finally {
+      setState(() {
+        _isRegistering = false;
+        });
+    }
+    }
 }
